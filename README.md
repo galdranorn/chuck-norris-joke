ec15e04f0bc5827c8c45d331e866292b06074016
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-05-15 19:23:29 +0200

    Update packages and modify readME.md

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # chuck-norris-joke
-First ajax exercise
+Chuck Norris's random joke machine. Basic exercise, just to practice AJAX.
+**You can the live [here](https://galdranorn.github.io/chuck-norris-joke/).**
+
+## Technologies used
+  - HTML
+  - CSS (Sass)
+  - AJAX
+
+## Useful scripts
+- If you want to work with this project locally, download this repository and run `npm install`
+- You can watch your changes during work by running `npm watch`
+- If you're finished, you can build production version by running `npm build`
+
+## Other
+  - [API I used](https://api.icndb.com/jokes/random)
+
--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 // links to html
 
-const url = 'http://api.icndb.com/jokes/random';
+const url = 'https://api.icndb.com/jokes/random';
 const button = document.getElementById('get-joke');
 const paragraph = document.querySelector('#joke');
 
 // event listener for get random joke button
-button.addEventListener('click', function(){
-    getJoke();
-  });
+button.addEventListener('click', function () {
+  getJoke();
+});
 
 // get random joke function
 function getJoke() {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.addEventListener('load', function(){
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.addEventListener('load', function () {
     // JSON parsed to JS object
-      let response = JSON.parse(xhr.response);
+    let response = JSON.parse(xhr.response);
     // getting to the joke in response
-      paragraph.innerHTML = response.value.joke;
-    });
-    xhr.send();
-  }
+    paragraph.innerHTML = response.value.joke;
+  });
+  xhr.send();
+}
 
 // when page loads one random joke already on place
 getJoke();
